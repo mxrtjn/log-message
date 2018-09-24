3b3e193592e0e55e84e33b0c1a208e0fe74fe8ba
mxrtjn <martin.pacora@gmail.com>
2018-09-24 00:43:08 -0500

    adding solution

---
+++ Py.Logger/Py.Logger/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Py.Logger
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
